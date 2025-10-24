7b3628cb504fd684f674029dd19022d9c5115db3
Ahan Anupam <ahananupam@gmail.com>
2025-10-25 00:24:20 +0530

    Added middleware with arcjet

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.routes.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 const app = express();
 
@@ -12,12 +13,13 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   morgan('combined', {
     stream: { write: message => logger.info(message.trim()) },
   })
 );
-app.use(cookieParser());
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions!');
